50f6dc21e058cc04ddc167759c93a61fec4e9882
Ian Reineck <ianreineck@gmail.com>
2020-05-09 10:33:02 -0500

    inital commit

---
+++ JobApplicationExample/JobApplicationExample/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JobApplicationExample.Startup))]
+namespace JobApplicationExample
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
